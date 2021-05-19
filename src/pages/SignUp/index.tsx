e986a451bc88ef64b13174fee1a9161d4ac99d55
William Brito <williamrldb@gmail.com>
2021-05-19 10:14:49 -0400

    --Criando tooltip de erros

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -15,7 +15,7 @@ import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
-  // eslint-disable-next-line @typescript-eslint/ban-types
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
       formRef.current?.setErrors({});
